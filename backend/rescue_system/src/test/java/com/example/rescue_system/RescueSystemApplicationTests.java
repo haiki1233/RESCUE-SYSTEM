4460fb403db8273da8d587c2b85767f91f81a531
sang <2251120047@ut.edu.vn>
2026-04-25 00:09:57 +0700

    chore: initialize backend (spring boot) and frontend (react ts) projects

---
+++ backend/rescue_system/src/test/java/com/example/rescue_system/RescueSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rescue_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RescueSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
